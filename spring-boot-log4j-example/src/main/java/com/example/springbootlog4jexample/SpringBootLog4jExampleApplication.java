fd307997f797528f64aeadaa05328362b23ac395
Phuc Tran <phuc.tran@codestringer.com>
2020-07-08 18:15:03 +0700

    commit

---
+++ spring-boot-log4j-example/src/main/java/com/example/springbootlog4jexample/SpringBootLog4jExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlog4jexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLog4jExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-log4j-example/src/test/java/com/example/springbootlog4jexample/SpringBootLog4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlog4jexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLog4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
