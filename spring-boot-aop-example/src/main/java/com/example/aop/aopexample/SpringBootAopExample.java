91b50448551179cc0c7f29e7143ea8dc67ff2ad6
Phuc Tran <tranthienphuc0102@gmail.com>
2020-09-09 15:17:17 +0700

    commit

--- spring-boot-aop-example/src/main/java/com/example/aop/aopexample/SpringBootAopExample.java
+++ spring-boot-aop-example/src/main/java/com/example/aop/aopexample/SpringBootAopExample.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AopExampleApplication {
+public class SpringBootAopExample {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AopExampleApplication.class, args);
+		SpringApplication.run(SpringBootAopExample.class, args);
 	}
 
 }
